4ef8a1b3d85739d68badf1ff2938d0d29b3368cd
Aziz Sobirov <azizsobirovdev@gmail.com>
2021-09-29 15:05:18 +0500

    1

--- js/quizes.js
+++ js/quizes.js
@@ -55,61 +55,31 @@ let quizes = [
             option2: "take",
             option3: "took"
         }
+    },{
+        quiz:'There _____ 4 _____ on the sofa?',
+        answer: "are / women",
+        options: {
+            option1: "are / woman",
+            option2: "is / women",
+            option3: "are / women"
+        }
+    },{
+        quiz:'_____ any men in the hall?',
+        answer: "Are there",
+        options: {
+            option1: "Are there",
+            option2: "Are their",
+            option3: "Is there"
+        }
+    },{
+        quiz:' Our homework ____ very difficult yesterday, but today it ____ easy',
+        answer: "was / is",
+        options: {
+            option1: "is / is",
+            option2: "was / is",
+            option3: "was / was"
+        }
     }
 ]
 
 
-let quiz = document.querySelector('.app_quiz')
-let indexQuiz = 0
-let score = 0
-
-function loadQuiz(i){
-    console.log(score);
-    quiz.innerHTML=`
-    <div class="quiz-box">
-        <h3>${quizes[i].quiz}</h3>
-        <form action="">
-            <label for="opt1">
-                <input type="radio" class="quiz_opt" name="opt" id="opt1" value="${quizes[i].options.option1}">
-                <h4>${quizes[i].options.option1}</h4>
-            </label>
-            <label for="opt2">
-                <input type="radio" class="quiz_opt" name="opt" id="opt2" value="${quizes[i].options.option2}">
-                <h4>${quizes[i].options.option2}</h4>
-            </label>
-            <label for="opt3">
-                <input type="radio" class="quiz_opt" name="opt" id="opt3" value="${quizes[i].options.option3}">
-                <h4>${quizes[i].options.option3}</h4>
-            </label>
-        </form>
-        <div id="as_btn" onclick="nextQuiz(${i})"><a href="#">Next</a></div>
-    </div>`
-}loadQuiz(indexQuiz)
-
-function nextQuiz(val){
-    let quiz_bx = document.querySelector(".quiz-box")
-    let quiz_inp = document.querySelectorAll('.quiz_opt')
-    for(let i=0;i<quiz_inp.length;i++){
-        if(quiz_inp[i].checked && quiz_inp[i].value == quizes[val].answer){
-            score += 100
-        }
-    }
-
-    if(quizes.length - 1 == indexQuiz){
-        let resultQuiz = document.querySelector(".app_result");
-        let ansCorrect = score / 100;
-        let ansUnCorrect = quizes.length - ansCorrect;
-
-        resultQuiz.innerHTML=`
-<div class="quiz-box">
-<h4>All ${quizes.length} tests</h4><br>
-<h3>Your Score: ${score}ball</h3><br>
-<h4>Correct answers: ${ansCorrect}</h4>
-<h4>Uncorrect answers: ${ansUnCorrect}</h4>
-</div>`
-        started()
-    }else{
-        indexQuiz++
-        loadQuiz(indexQuiz)
-    }
-}
